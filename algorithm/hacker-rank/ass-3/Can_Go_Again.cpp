753b9a20ea1295e49e3de4fe9e1ec1e194110c9c
foysal41 <mdfoysal.mf33@gmail.com>
2025-09-05 21:09:13 +0600

    Shortest_Distance hr problem

--- algorithm/hacker-rank/ass-3/Can_Go_Again.cpp
+++ algorithm/hacker-rank/ass-3/Can_Go_Again.cpp
@@ -12,8 +12,8 @@ class Edge
     }
 };
 
-int dis[1005]; //04) source থেকে প্রতিটি নোড পর্যন্ত shortest distance রাখবে।
-vector<Edge> edge_list; //02 সব এজ জমা থাকবে এখানে।
+int dis[1005]; 
+vector<Edge> edge_list; 
 int n , e; 
 
 int main(){
@@ -22,7 +22,7 @@ int main(){
 
     
 
-    while(e--)//01) while থেকে shob লুপ চালিয়ে push back করে। edge_list মদ্দে রাখলাম। 
+    while(e--)
 
     {
         int a, b, c;
@@ -32,10 +32,10 @@ int main(){
     }
 
 
-    int source; //03) Bellman-Ford সব distance হিসাব করবে s থেকে।
+    int source; 
     cin >> source;
 
-    int qry; //05 query
+    int qry;
     cin >> qry;
     vector<int> query(qry);
     for(int i=0; i< qry; i++)
@@ -43,31 +43,20 @@ int main(){
         cin >> query[i];
     }
 
-    /*  Details in query;
-    qry = কয়টা query আসবে। প্রতিটি qry আসলে একটা destination node এর নাম্বার। অর্থাৎ: "source থেকে এই নোডে যাওয়ার shortest path কত?"
-    সব query-কে query[] ভেক্টরে রেখে দিলাম। 
-
-    example: ধরি source = 1 আর query list হলো 3 4 5।
-        1 থেকে 3 এ shortest distance কত?
-        1 থেকে 4 এ shortest distance কত?
-        1 থেকে 5 এ shortest distance কত?
-    */
 
     for(int i = 1; i<=n; i++)
     {
-        dis[i] = INT_MAX;  //06) শুরুতে সব distance = "অজানা" (INT_MAX) ধরা হলো।   
+        dis[i] = INT_MAX;  
     }
-    dis[source] = 0; //07) source node source থেকে source- এ যাওয়ার distance সবসময় 0 
+    dis[source] = 0; 
 
     for(int i=0; i<n-1; i++){
         for(auto ed : edge_list)
         {
-            //08) প্রতিটি edge (a → b, cost = c) চেক করা হয়।
             int a = ed.a;
             int b = ed.b;
             int c = ed.c;
 
-            //09) যদি source থেকে a তে যাওয়ার পথ জানা থাকে (dis[a] != INT_MAX) এবং dis[a] + c এর মান ছোট হয় dis[b] থেকে → তাহলে dis[b] আপডেট করা হয়।
             if(dis[a] != INT_MAX && dis[a] + c < dis[b])
             {
                 dis[b] = dis[a] + c;
@@ -79,12 +68,10 @@ int main(){
     bool cycle = false;
     for(auto ed: edge_list){
 
-        //10) আবার একবার সব edge চেক করা হয়।
          int a = ed.a;
          int b = ed.b;
          int c = ed.c; 
 
-         //11)যদি আরও ছোট distance পাওয়া যায়, তাহলে এর মানে negative cycle আছে।
          if(dis[a] != INT_MAX && dis[a] + c < dis[b])
          {
             cycle = true;
@@ -92,7 +79,6 @@ int main(){
          }
     }
 
-//12) যদি negative cycle থাকে → সরাসরি বলে দেয় "Negative Cycle Detected"।
     if(cycle)
     {
         cout << "Negative Cycle Detected" << endl;
@@ -100,7 +86,6 @@ int main(){
 
     }
 
-    //13)না হলে প্রতিটি query node এর shortest distance প্রিন্ট করে। যদি কোনো node এ যাওয়া সম্ভব না হয় → "Not Possible"
     for(int d : query)
     {
         if(dis[d] == INT_MAX)
